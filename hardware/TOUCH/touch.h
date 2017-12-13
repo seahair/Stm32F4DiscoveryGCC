2bf56d681ac87093180fcfd13e73f3fbd0d55daf
beck <guolinxin@shmingjiang.net>
2017-12-13 18:11:55 +0800

    add touch

---
+++ hardware/TOUCH/touch.h
@@ -0,0 +1,22 @@
+#ifndef  __TOUCH_H__
+#define  __TOUCH_H__
+
+#include "stm32f4xx.h"
+
+typedef struct _TOUCH_ATTR{
+	u16  x;
+	u16  y;
+	u8 dir;
+}TOUCH_ATTR;
+
+typedef struct _TOUCH_DIR{
+	s8 (*init) ( TOUCH_ATTR *ptouchattr );
+	s8 (*match) ( void );
+	void (*getxy) ( u16 *x, u16 *y );
+}TOUCH_DIR;
+
+s8 TouchInit( TOUCH_ATTR *ptouchattr );
+s8 TouchMatch( void );
+void TouchGetxy( u16 *x, u16 *y );
+
+#endif
