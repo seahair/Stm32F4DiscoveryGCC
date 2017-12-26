8871a456722da4106f042029de52dc5d48b1db94
beck <guolinxin@shmingjiang.net>
2017-12-26 10:52:16 +0800

    change asm in sys.c

--- src/sys.c
+++ src/sys.c
@@ -29,33 +29,34 @@ __asm void MSR_MSP(u32 addr)
 #endif
 
 
+#define ATTR_NO_INSTRUMENT_FUNCTION __attribute__( ( no_instrument_function ) )
 
 //THUMB指令不支持汇编内联
 //采用如下方法实现执行汇编指令WFI  
-void WFI_SET(void)
+inline ATTR_NO_INSTRUMENT_FUNCTION void WFI_SET(void)
 {
-	__asm__("WFI");		  
+	__asm__ __volatile__ ("WFI");		  
 }
 //关闭所有中断(但是不包括fault和NMI中断)
-void INTX_DISABLE(void)
+inline ATTR_NO_INSTRUMENT_FUNCTION void INTX_DISABLE(void)
 {
-	__asm__("CPSID   I"); 
-	__asm__("BX  LR");	  
+	__asm__ __volatile__ ("CPSID   I"); 
+	__asm__ __volatile__ ("BX  LR");	  
 	
 }
 //开启所有中断
-void INTX_ENABLE(void)
+inline ATTR_NO_INSTRUMENT_FUNCTION void INTX_ENABLE(void)
 {
-	__asm__("CPSIE   I"); 
-	__asm__("BX LR");  
+	__asm__ __volatile__ ("CPSIE   I"); 
+	__asm__ __volatile__ ("BX LR");  
 	
 }
 //设置栈顶地址
 //addr:栈顶地址
-void MSR_MSP(u32 addr) 
+inline ATTR_NO_INSTRUMENT_FUNCTION void MSR_MSP(u32 addr) 
 {
-	__asm__("MSR MSP, r0");	 
-	__asm__("BX r14");
+	__asm__ __volatile__ ("MSR MSP, r0");	 
+	__asm__ __volatile__ ("BX r14");
 	
 }
 
